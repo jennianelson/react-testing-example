879759c3205961bbafa9e7efc8d210c0379ebf15
Jennifer Nelson <jennifernelson118@gmail.com>
2018-10-23 09:57:24 -0500

    App.test.js: configured to use react-testing-library

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
+// react-testing-library renders your components to document.body,
+// this will ensure they're removed after each test.
+import 'react-testing-library/cleanup-after-each';
+// this adds jest-dom's custom assertions
+import 'jest-dom/extend-expect';
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-testing-library';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders welcome message', () => {
+  const { getByText } = render(<App />);
+  expect(getByText('Learn React')).toBeInTheDocument();
 });
