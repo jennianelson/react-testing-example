cb49dafd69c05a6214b9799d6b98ff89bc685161
Jennifer Nelson <jennifernelson118@gmail.com>
2018-10-25 10:25:33 -0500

    login.test: added snapshot

--- src/_tests_/login.test.jsx
+++ src/_tests_/login.test.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+// import { generate } from 'til-client-test-utils'
+import { render } from 'react-testing-library'
 import ReactDOM from 'react-dom'
 import Login from '../components/login'
 
+//test without abstraction
 test('calls onSubmit with the username and password when submitted', () => {
   const handleSubmit = jest.fn()
   const container = document.createElement('div')
@@ -19,4 +22,29 @@ test('calls onSubmit with the username and password when submitted', () => {
     username: username.value,
     password: password.value,
   })
+})
+// test with abstraction -- what is this generate fn()?
+// test('calls onSubmit witht he username and password when submitted', () => {
+//   // ARRANGE
+//   const fakeUser = generate.loginForm()
+//   const handleSubmit = jest.fn()
+//   const { getByLabelText, getByText } = renderintoDocument(
+//     <Login onSubmit={handleSubmit} />,
+//   )
+//   const usernameNode = getByLabelText('username')
+//   const passwordNode = getByLabelText('password')
+
+//   // ACT
+//   usernameNode.value = fakeUser.username
+//   passwordNode.value = fakeUser.password
+//   getByText('submit').click()
+
+//   // ASSERT
+//   expect(handleSubmit).toHaveBeenCalledTimes(1)
+//   expect(handleSubmit).toHaveBeenCalledWith(fakeUser)
+// })
+
+test('snapshot', () => {
+  const { container } = render(<Login onSubmit={() => {}} />)
+  expect(container.firstChild).toMatchSnapshot()
 })
