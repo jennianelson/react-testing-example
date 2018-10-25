d80eedffa032e69cdf2fa489b304b4fe05a4102b
Jennifer Nelson <jennifernelson118@gmail.com>
2018-10-25 09:39:31 -0500

    login: created login component

---
+++ src/components/login.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Login = ({ onSubmit }) => {
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          const { username, password } = e.target.elements
+          onSubmit({
+            username: username.value,
+            password: password.value,
+          })
+        }}
+      >
+        <input type="text" placeholder="username" name="username" />
+        <input type="password" placeholder="password" name="password" />
+        <input type="submit" />
+      </form>
+    </div>
+  )
+}
+
+export default Login
